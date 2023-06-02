012ef0ccbb1ab6cb30f794776322edf4c4f58a36
xiaobaihacker <xiaobaihacker@users.noreply.github.com>
2023-06-02 17:35:43 +0000

    deploy: c253bc72b71f96fc6cd3f128a025c082e0c0bbe6

--- main.dart.js
+++ main.dart.js
@@ -27676,7 +27676,7 @@ $1(a){var s=$.dk
 s=(s==null?$.dk=A.iH(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/90fa3ae28fe6ddaee1af2c120f01e50201c1401b/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/2a3401c9bbb5a9a9aec74d4f735d18a9dd3ebf2d/":s)+a},
 $S:86}
 A.arf.prototype={
 $1(a){this.a.remove()
