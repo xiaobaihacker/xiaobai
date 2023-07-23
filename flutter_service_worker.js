daf93461433629a4865fa3116033aeba83a34b09
xiaobaihacker <xiaobaihacker@users.noreply.github.com>
2023-07-23 17:35:47 +0000

    deploy: c253bc72b71f96fc6cd3f128a025c082e0c0bbe6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "b27f3e5b6d6cf9566a5fa10acf5211b0",
-"/": "b27f3e5b6d6cf9566a5fa10acf5211b0",
+const RESOURCES = {"index.html": "62fc380f9a108aeaefcdd58dbc99cd17",
+"/": "62fc380f9a108aeaefcdd58dbc99cd17",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "27ca9af8c6f5952f10f54d642cf9ae76",
